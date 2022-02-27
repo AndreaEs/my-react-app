859f554434747e64785ece14c3b6fdc52ba9cda7
Andrea Escribano <andreaesgar.detec@gmail.com>
2022-02-27 13:15:20 +0100

    Call this.setState from Another Function

---
+++ src/states/Toggle.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render (){
+        return (
+            <div>
+                <h1>
+                    Change my color
+                </h1>
+            </div>
+        );
+    }
+}
