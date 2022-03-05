ca1e3c2149480d5699940310170611847c004815
Andrea Escribano <andreaesgar.detec@gmail.com>
2022-03-05 09:34:46 +0100

    componentWillUnmount

--- src/lifeCycle/Clock.js
+++ src/lifeCycle/Clock.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import { ReactDOM } from 'react';
 
 export class Clock extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { date: new Date() };
-      }
+        this.state = { 
+            date: new Date(),
+            isClockVisible: true
+         };
+         this.toggleClock = this.toggleClock.bind(this);
+    }
+    toggleClock(){
+        this.setState({isClockVisible: !this.state.isClockVisible});
+    }
 
-      render() {
-        return <div>{this.state.date.toLocaleTimeString()}</div>;
-      }
+    render() {
+        let localDate;
+        if(this.state.isClockVisible){
+            localDate = this.state.date.toLocaleTimeString();
+        }
 
-      componentDidMount(){
+        return (
+        <div>
+            <button onClick={this.toggleClock}>
+                Toggle clock
+            </button>
+            {localDate}
+        </div>);
+    }
+
+    componentDidMount(){
         const oneSecond = 1000;
-        setInterval(() =>{
+        this.intervalID = setInterval(() =>{
           this.setState({
             date: new Date()
           });
         }, oneSecond);
-      }
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalID);
+    }
 }
 
 export default Clock;
